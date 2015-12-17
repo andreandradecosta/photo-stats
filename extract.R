5f8ce89041ff9b843a791f71b3cb274c6937e263
Andre Costa <andre.andrade.costa@gmail.com>
2015-12-17 20:21:40 -0200

    Adjust cmd line par of exiftool.

--- extract.R
+++ extract.R
@@ -1,7 +1,8 @@
 source("init.R")
+dir.create("data", showWarnings = FALSE)
 folders<-c("/Volumes/Pictures/2012", "/Volumes/Pictures/2013", "~/Pictures/2014", "~/Pictures/2015")
 for (f in folders) {
   print(f)
   outFilename <- paste0("data", "/", basename(f), ".csv")
-  system(paste("exiftool -if '$focallength' -if '$model eq \"Canon EOS REBEL T3\"' -csv -q -s -R -ISO -createdate -aperture -shutterspeed -focallength -lens -ext CR2 -ext JPG", f, ">", outFilename))
+  system(paste("exiftool -if '$focallength' -if '$model eq \"Canon EOS REBEL T3\"' -csv -q -s -R -ISO -createdate -aperture -shutterspeed -focallength# -lensmodel -ext CR2 -ext JPG", f, ">", outFilename))
 }
